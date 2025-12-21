e02ac21b2d69c6a598d36979cadd6734ceb06a2a
sqmmi3 <samip.shrestha@student.ucll.be>
2025-12-21 18:00:00 +0100

    FIX [fix_fetching_user_items]: 1. backend DTO returns different object, so model should be adapted

--- lib/models/user_item.dart
+++ lib/models/user_item.dart
@@ -1,13 +1,17 @@
 class UserItem {
   final int id;
+  final int itemId;
   final String itemName;
+  final String type;
   final DateTime expirationDate;
   final DateTime? openedDate;
   final int? openedRule;
 
   UserItem({
     required this.id,
+    required this.itemId,
     required this.itemName,
+    required this.type,
     required this.expirationDate,
     this.openedDate,
     this.openedRule,
@@ -16,7 +20,9 @@ class UserItem {
   factory UserItem.fromJson(Map<String, dynamic> json) {
     return UserItem(
       id: json['id'],
-      itemName: json['item']['name'],
+      itemId: json['itemId'],
+      itemName: json['itemName'],
+      type: json['type'],
       expirationDate: DateTime.parse(json['expirationDate']),
       openedDate: json['openedDate'] != null ? DateTime.parse(json['openedDate']) : null,
       openedRule: json['openedRule'],
